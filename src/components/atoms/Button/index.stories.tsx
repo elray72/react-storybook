3e51a540ccb2be2d25381d1daecb302790f50620
Ray Ranola <ray.ranola@vmlyr.com>
2019-08-01 15:34:30 +1000

    feature: (button: stage 1 complete)

--- src/components/atoms/Button/index.stories.tsx
+++ src/components/atoms/Button/index.stories.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import Story from '../../templates/Story';
-import Component from './index';
+import Story, { StoryContent, StoryDemo, StoryItem, StoryLayout } from '../../templates/Story';
+import Button from './index';
 
 const StoryComponent = () => (
 	<Story>
-		<Component />
+		<StoryContent>
+			<h1>Button</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et in, labore sapiente sint tenetur voluptate!</p>
+		</StoryContent>
+		<StoryDemo layout={StoryLayout.Rows}>
+			<StoryItem label="Primary">
+				<Button className="button" theme="primary">Primary</Button>
+			</StoryItem>
+			<StoryItem label="Secondary">
+				<Button className="button" theme="secondary">Secondary</Button>
+			</StoryItem>
+			<StoryItem label="Tertiary">
+				<Button className="button" theme="tertiary">Tertiary</Button>
+			</StoryItem>
+		</StoryDemo>
 	</Story>
 );
 
