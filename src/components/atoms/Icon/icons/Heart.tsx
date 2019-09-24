c8a38a585b17a5eb1317c058c31607d45956464e
Ray Ranola <ray@sypht.com>
2019-09-24 13:31:19 +1000

    feature: (icons: fixes to heart)

--- src/components/atoms/Icon/icons/Heart.tsx
+++ src/components/atoms/Icon/icons/Heart.tsx
@@ -3,7 +3,11 @@ import Icon, { IIcon } from '../Icon';
 import svg from '../svg/heart.svg';
 import svgO from '../svg/heart-o.svg';
 
-export const Heart: React.FC<IIcon> = (props) => {
+export interface IHeart extends IIcon {
+	variant?: undefined | 'o';
+}
+
+export const Heart: React.FC<IHeart> = (props) => {
 	const _svg = props.variant === 'o' ? svgO : svg;
 	return <Icon src={_svg} {...props} />;
 };
